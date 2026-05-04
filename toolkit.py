0fb87998343a8cd4f238754e3921004873770985
jan <kluthjan@gmail.com>
2026-05-04 11:01:07 +0200

    feat: upgrade to v0.2 EN

--- toolkit.py
+++ toolkit.py
@@ -1,6 +1,5 @@
-# Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
-# Aufgabe es verlangt!
-VERSION = "0.1"
+return "Mini-Toolkit v0.2 - Hello from Team C"
+VERSION = "0.2"
 
 def banner():
     # >>> KONFLIKTBEREICH: Hier werden zwei Personen
