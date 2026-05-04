bdcbdd4bb1d0f682de76883f7fdc0e479192086b
jan <kluthjan@gmail.com>
2026-05-04 10:44:41 +0200

    startpunkt für projekt

---
+++ toolkit.py
@@ -0,0 +1,11 @@
+# Mini-Toolkit – bitte NICHT am Header ändern, außer wenn die
+# Aufgabe es verlangt!
+VERSION = "0.1"
+
+def banner():
+    # >>> KONFLIKTBEREICH: Hier werden zwei Personen
+    # gleichzeitig ändern <<<
+    return "Mini-Toolkit v0.1 - Welcome"
+
+if __name__ == "__main__":
+    print(banner())
